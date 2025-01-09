8f0157f9a16f702abfba3d9c1cd20c3ef4fe4c41
Yan Pires <92273343+YanSP1@users.noreply.github.com>
2025-01-09 16:40:59 -0300

    Add files via upload

---
+++ Javascript/Basic operations/class1(text).js
@@ -0,0 +1,37 @@
+// Este é um comentário (Ctr + K /Ctrl + C)
+
+/*
+Este é um comentário em bloco.
+Feito com
+*/
+
+//Hello  world
+// console.log("Hello  world");
+
+//Name
+// let name = "Yan"
+// console.log('nome do individuo: ' + name);
+
+//Name + number
+// console.log('Meu nome é Yan e estou na aula', 1, 'de javascript.');
+
+//Name + let/var
+// let name = 'João'
+// var date = 2000
+// console.log(name, 'não nasceu em', date, '.');
+// console.log('Mas em', date , name, 'conheceu Maria.')
+
+//Teste com erro de let e var com testo
+/*
+    function teste(){
+        if(true){
+            var/let nome="Yan"
+            console.log("Dentro do IF: " + nome) //correct com let e var
+        }
+        console.log("Dentro do teste: " + nome) //erro com let / correct com var 
+    }
+
+    teste()
+
+    console.log("Fora do teste: " + nome) //erro com let e var
+*/
---
+++ Javascript/Basic operations/class4.js
@@ -0,0 +1,6 @@
+let 
+num1=47
+num2=6
+
+res=num1+num2
+console.log(res)
