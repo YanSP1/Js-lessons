6913d63fd1ad1040efabec888338f3cebf68c019
Yan Pires <92273343+YanSP1@users.noreply.github.com>
2025-02-07 16:58:00 -0300

    Delete Javascript/Basic operations directory

--- Javascript/Basic operations/class4.js
+++
@@ -1,6 +0,0 @@
-let 
-num1=47
-num2=6
-
-res=num1+num2
-console.log(res)
--- Javascript/Basic operations/class6.js
+++
@@ -1,9 +0,0 @@
-// let n1 = 1
-// let n2 = "1"
-
-// let v1 = {nome:"Bruno"}
-// let v2 = v1
-
-// let nome = prompt("Digite seu nome")
-
-// console.log(nome)
