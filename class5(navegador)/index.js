3671aa5dc56676a59e9ac5d64f2d0e75d3d1d5c5
Yan Pires <92273343+YanSP1@users.noreply.github.com>
2025-02-07 17:05:42 -0300

    Add files via upload

---
+++ class5(navegador)/index.js
@@ -0,0 +1,23 @@
+//console.log('Hello world')
+//alert('EI!') = window.alert('Mensagem inicial.');
+//window.confirm('Quer continuar?');
+//window.prompt('Digite o seu nome');
+//confirm('Deseja apagar?');
+
+//const confirma = confirm('Você confirma?');
+//console.log(confirma) visualise no console do inspecionar do navegador
+
+/*let num1 = prompt('Digite um número');
+let num2 = prompt('Digite um número');
+const resultado = num1 * num2;
+alert(resultado);
+
+console.log(typeof(resultado))*/
+
+let num1 = prompt('Digite um número:');
+let num2 = prompt('Digite um número:');
+
+/*num1 = Number(num1);
+num2 = Number(num2);*/
+
+alert(`O resultado da sua conta foi: ${num1 + num2}`);
