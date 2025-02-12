918c98f104458ab932feb5c16d3f192a50620873
Yan Pires <92273343+YanSP1@users.noreply.github.com>
2025-02-12 14:35:28 -0300

    Update index.js

--- class5(navegador)/index.js
+++ class5(navegador)/index.js
@@ -14,10 +14,27 @@ alert(resultado);
 
 console.log(typeof(resultado))*/
 
-let num1 = prompt('Digite um número:');
-let num2 = prompt('Digite um número:');
+// let num1 = prompt('Digite um número:');
+// let num2 = prompt('Digite um número:');
 
 /*num1 = Number(num1);
 num2 = Number(num2);*/
 
-alert(`O resultado da sua conta foi: ${num1 + num2}`);
+//alert(`O resultado da sua conta foi: ${num1 + num2}`);
+
+let varA = 'A'; //B
+let varB = 'B'; //C
+let varC = 'C'; //A
+
+// varA = prompt('Digite um número:');
+// varB = prompt('Digite um número:');
+// varC = prompt('Digite um número:');
+
+// const varATemp= varA
+// varA = varA
+// varB = varC
+// varC = varATemp
+
+[varA, varB, varC] = [varB, varC, varA]
+
+console.log(varA, varB, varC)
