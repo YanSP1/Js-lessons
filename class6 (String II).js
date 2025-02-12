2a163a07a086ea5ea10a88b8ed66f0926ccb7d91
Yan Pires <92273343+YanSP1@users.noreply.github.com>
2025-02-12 14:34:04 -0300

    Add files via upload

---
+++ class6 (String II).js
@@ -0,0 +1,11 @@
+//               01234567
+let umaString = "um texto";
+
+// console.log(umaString.concat(' ', 'em', ' ', 'um'));
+// console.log(umaString.concat(' em', ' um', ' lindo dia.'));
+// console.log(umaString + ' em um lindo dia.');
+// console.log(`${umaString} em um lindo dia.`);
+
+console.log(umaString.indexOf('texto'));
+console.log(umaString.indexOf('o', 3));
+console.log(umaString.lastIndexOf('o', 3));
